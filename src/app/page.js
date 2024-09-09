f8b35a554b269dde40bbf57f62b06c9dc2a95a1d
Tristan H <tristanskyhook@gmail.com>
2024-09-09 12:44:27 -0700

    display date

--- src/app/page.js
+++ src/app/page.js
@@ -7,18 +7,22 @@ import { useState, useEffect } from 'react';
 export const dynamic = 'force-static';
 export default function Home() {
   const [allData, setAllData] = useState({});
+  const [lastUpdated, setLastUpdated] = useState('Unknown');
+  const [showLastUpdated, setShowLastUpdated] = useState(true);
 
   useEffect(() => {
     if (Object.keys(allData).length === 0) {
-      getData().then((data) => {
-        setAllData(data);
+      getData().then((a) => {
+        setAllData(a[0]);
+        setLastUpdated(a[1]);
+        setShowLastUpdated(a[2]);
       });
     }
   }, []);
 
   return (
     <main>
-      <Container allData={allData} />
+      <Container allData={allData} lastUpdated={lastUpdated} showLastUpdated={showLastUpdated} />
     </main>
   );
 }
